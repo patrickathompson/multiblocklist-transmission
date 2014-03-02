b67b40c8f6bd4ea2a8ee167df2a75369423ee5a3
patrick <patrick@comparemetrics.com>
2014-03-02 17:26:51 -0600

    first commit

---
+++ multiblocklist-updater.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo && echo "Updating Bitorrent P2P Blocklists..."
+
+
+rm -f blocklists/*.bin
+
+count=1
+
+for line in $(cat multiblocklist.conf); do
+	echo && echo "Downloading List $count..."
+	if wget -q -Oblocklists/$count.gz $line; then
+		echo "Download Complete!"
+		gunzip -fN blocklists/$count.gz && rm -f blocklists/$count.gz
+		echo "Extraction of List $count completed!"
+	else
+		echo "The following url:"
+		echo $line
+		echo "FAILED to download!!!"
+	fi;
+	let count=count+1
+done;
+
+echo && echo "Bitorrent P2P Blocklists Update Complete!!!" && echo
+
+exit 0
